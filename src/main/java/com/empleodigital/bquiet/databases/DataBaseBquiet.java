234d8a4dbb491d3808db72e9e6bd5ffcaa2c2a78
ivan <ivaann2706@gmail.com>
2017-03-02 11:30:01 +0100

    metodo listaUsuarios en databasebquiet

--- src/main/java/com/empleodigital/bquiet/databases/DataBaseBquiet.java
+++ src/main/java/com/empleodigital/bquiet/databases/DataBaseBquiet.java
@@ -1,6 +1,11 @@
 package com.empleodigital.bquiet.databases;
 
+import java.util.ArrayList;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.empleodigital.bquiet.beans.Usuario;
 
 public class DataBaseBquiet extends DataBaseGenerica {
 	private JdbcTemplate jdbc;
@@ -11,6 +16,14 @@ public class DataBaseBquiet extends DataBaseGenerica {
 		this.jdbc = new JdbcTemplate(Conector.getDataSource());
 	}
 	
+	public ArrayList<Usuario> listaUsuarios(){
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+		
+		String sql = "SELECT * FROM usuarios";
+		usuarios = (ArrayList<Usuario>)jdbc.query(sql, new BeanPropertyRowMapper<Usuario>(Usuario.class));
+		
+		return usuarios;
+	}
 	
 	
 }
