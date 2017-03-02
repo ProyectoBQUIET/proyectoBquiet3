246bb6a555f5e7b8481ea44989c23e2a6c4d0219
JavaCristian <java_cristian@gmx.es>
2017-03-02 09:24:30 +0100

    agrego el script SQL

---
+++ bquiet.sql
@@ -0,0 +1,96 @@
+/**** CREACION DE LA BASE DE DATOS ****/
+
+DROP DATABASE IF EXISTS bquiet;
+CREATE DATABASE IF NOT EXISTS bquiet;
+
+USE bquiet;
+
+/**** CREACION DE TABLAS ****/
+
+CREATE TABLE tipousuario (
+	id INT(1) AUTO_INCREMENT  NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50) UNIQUE NOT NULL
+
+)ENGINE = INNODB;
+
+CREATE TABLE usuarios (
+	id INT(5) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	id_tipousuario INT(1) NOT NULL,
+	nombre VARCHAR(50) UNIQUE NOT NULL,
+	FOREIGN KEY (id_tipousuario) REFERENCES tipousuario (id)
+
+) ENGINE=INNODB;
+
+CREATE TABLE centros (
+	id INT(5) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50) NOT NULL
+
+) ENGINE = INNODB;
+
+CREATE TABLE cursos (
+	id INT(5) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50) NOT NULL,
+	id_centro INT(5) NOT NULL,
+	FOREIGN KEY (id_centro) REFERENCES centros (id)
+
+) ENGINE = INNODB;
+
+CREATE TABLE aulas (
+	id INT(5) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	nombre VARCHAR(50),
+	id_centro INT(5) NOT NULL,
+	FOREIGN KEY (id_centro) REFERENCES centros (id)
+
+) ENGINE = INNODB;
+
+CREATE TABLE usuarios_centros (
+	id INT(5) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	id_usuario INT(50) NOT NULL,
+	id_centro INT(5) NOT NULL,
+	FOREIGN KEY (id_usuario) REFERENCES usuarios (id),
+	FOREIGN KEY (id_centro) REFERENCES centros (id)
+
+) ENGINE = INNODB;
+
+CREATE TABLE registros (
+	id INT(5) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	fecha DATE NOT NULL,
+	hora INT(2) NOT NULL,
+	minima INT(5) NOT NULL,
+	maxima INT(5) NOT NULL,
+	media INT(5) NOT NULL,
+	excesos INT(5) NOT NULL,
+	id_aula INT(5) NOT NULL,
+	id_curso INT(5) NOT NULL,
+	id_usuario INT(5) NOT NULL,
+	id_centro INT(5) NOT NULL,
+	FOREIGN KEY (id_aula) REFERENCES aulas (id),
+	FOREIGN KEY (id_curso) REFERENCES cursos (id),
+	FOREIGN KEY (id_usuario) REFERENCES usuarios (id),
+	FOREIGN KEY (id_centro) REFERENCES centros (id)
+
+) ENGINE = INNODB;
+
+/**** INSERCION EN TABLAS ****/
+
+INSERT INTO tipousuario (nombre) VALUES ('administrador');
+INSERT INTO tipousuario (nombre) VALUES ('superusuario');
+INSERT INTO tipousuario (nombre) VALUES ('usuario');
+
+INSERT INTO usuarios (id_tipousuario, nombre) VALUES (1, 'El primer administrador');
+INSERT INTO usuarios (id_tipousuario, nombre) VALUES (2, 'El primer superusuario');
+INSERT INTO usuarios (id_tipousuario, nombre) VALUES (3, 'El primer usuario');
+
+INSERT INTO centros (nombre) VALUES ('La Termica');
+
+INSERT INTO cursos (nombre, id_centro) VALUES ('Cursjo de Java', 1);
+INSERT INTO cursos (nombre, id_centro) VALUES ('Cursjo de Apps', 1);
+
+INSERT INTO aulas (nombre, id_centro) VALUES ('Colmenar 1', 1);
+INSERT INTO aulas (nombre, id_centro) VALUES ('Colmenar 2', 1);
+INSERT INTO aulas (nombre, id_centro) VALUES ('Colmenar 3', 1);
+
+INSERT INTO usuarios_centros (id_usuario, id_centro) VALUES (1, 1);
+
+INSERT INTO registros (fecha, hora, minima, maxima, media, excesos, id_aula, id_curso, id_usuario, id_centro) VALUES (CURRENT_DATE, 13, 10, 50, 30, 4, 2, 1, 2, 1);
+INSERT INTO registros (fecha, hora, minima, maxima, media, excesos, id_aula, id_curso, id_usuario, id_centro) VALUES (CURRENT_DATE, 14, 12, 54, 32, 6, 2, 1, 2, 1);
