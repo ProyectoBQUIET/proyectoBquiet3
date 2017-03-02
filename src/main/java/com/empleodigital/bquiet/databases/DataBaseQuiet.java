03d25fd4bf0a1ee2148caa36b01608f632658aa9
mariajose <mj_merino@outlook.com>
2017-03-02 11:39:22 +0100

    creo el metodo listaColegios

--- src/main/java/com/empleodigital/bquiet/databases/DataBaseQuiet.java
+++ src/main/java/com/empleodigital/bquiet/databases/DataBaseQuiet.java
@@ -1,16 +1,30 @@
 package com.empleodigital.bquiet.databases;
 
+import java.util.ArrayList;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.empleodigital.bquiet.beans.Centro;
 
 public class DataBaseQuiet extends DataBaseGenerica {
 	private JdbcTemplate jdbc;
-	
+
 	public static final String BASE_DATOS = "reservas";
-	
+
 	public DataBaseQuiet(){
 		this.jdbc = new JdbcTemplate(Conector.getDataSource());
 	}
-	
-	
-	
+
+	public ArrayList<Centro> listaColegios(){
+
+		String sql = "SELECT * FROM centros";
+		ArrayList<Centro> listaCentros= (ArrayList<Centro>)this.jdbc.query(
+				sql, 
+				new BeanPropertyRowMapper<Centro>(Centro.class)
+				);
+
+		return listaCentros;
+	}
+
 }
